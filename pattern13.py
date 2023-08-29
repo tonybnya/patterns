f67436033da570af3711c2d5ccb92d2b98ae9766
Tony <nya.tony2010@gmail.com>
2023-08-29 12:48:44 +0100

    Update: pattern 13

--- pattern13.py
+++ pattern13.py
@@ -1,5 +1,7 @@
 # pylint: disable=all
 """
+Pattern 13:
+
 1
 2 3
 4 5 6
@@ -13,6 +15,6 @@ def pattern(n, char='*'):
 
     for i in range(1, n + 1):
         for j in range(i):
-            print(start, end='')
+            print(f'{start} ', end='')
             start += 1
         print()
