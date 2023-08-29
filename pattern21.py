eebf0949729c1a27154c67d12c4eebdd739f220f
Tony <nya.tony2010@gmail.com>
2023-08-29 14:36:18 +0100

    Update: pattern 21

--- pattern21.py
+++ pattern21.py
@@ -11,4 +11,24 @@ Pattern 21:
 
 
 def pattern(n, char='*'):
-    pass
+    space = ' '
+
+    for i in range(n):
+        if i == 0 or i == n - 1:
+            for j in range(n):
+                print(char, end='')
+        else:
+            # stars
+            print(char, end='')
+
+            # spaces
+            for j in range(n - 2):
+                print(space, end='')
+
+            # stars
+            print(char, end='')
+
+        print()
+
+
+pattern(9)
