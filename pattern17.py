7ef1540cfda5e0e743a0d3daf9a41653ec429708
Tony <nya.tony2010@gmail.com>
2023-08-29 13:32:06 +0100

    Update: pattern 17

--- pattern17.py
+++ pattern17.py
@@ -9,8 +9,26 @@ ABCDEDCBA
 
 
 def pattern(n, char='*'):
-    pass
+    space = ' '
 
+    for i in range(n):
+        # spaces
+        for j in range(n - i - 1):
+            print(space, end='')
 
-n = 5
-pattern(n)
+        # letters
+        for j in range(i * 2 + 1):
+            asc = 65
+
+            if j <= i:
+                code = asc + j
+                print(chr(code), end='')
+            else:
+                code -= 1
+                print(chr(code), end='')
+
+        # spaces
+        for j in range(n - i - 1):
+            print(space, end='')
+
+        print()
