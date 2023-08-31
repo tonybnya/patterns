0176dc32e35c90b523ad84e00449c3091f82e73b
Tony <nya.tony2010@gmail.com>
2023-08-31 08:48:00 +0100

    Update: pattern 20

--- pattern20.py
+++ pattern20.py
@@ -23,11 +23,26 @@ def pattern(n, char='*'):
             print(char, end='')
 
         # spaces
-        for j in range(n - 1 - i):
+        for j in range(n * 2 - i * 2):
             print(space, end='')
 
         # stars
         for j in range(i):
+            print(char, end='')
+
+        print()
+
+    for i in range(1, n):
+        # stars
+        for j in range(n - i):
+            print(char, end='')
+
+        # spaces
+        for j in range(i * 2):
+            print(space, end='')
+
+        # stars
+        for j in range(n - i):
             print(char, end='')
 
         print()
